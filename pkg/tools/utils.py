22aebc930ca0dbad7dfec4f0900192f47b49c0ee
Relakkes <relakkes@gmail.com>
2024-10-10 12:56:42 +0800

    fix: #60

--- pkg/tools/utils.py
+++ pkg/tools/utils.py
@@ -17,7 +17,8 @@ def init_loging_config():
     logging.basicConfig(
         level=level,
         format="%(asctime)s %(name)s %(levelname)s (%(filename)s:%(lineno)d) - %(message)s",
-        datefmt='%Y-%m-%d %H:%M:%S'
+        datefmt='%Y-%m-%d %H:%M:%S',
+        encoding='utf-8'
     )
     _logger = logging.getLogger("MediaCrawlerPro")
     _logger.setLevel(level)

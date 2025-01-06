030d2d3483701e1b825ff786077f441d93e1915a
Relakkes <relakkes@gmail.com>
2025-01-06 15:41:12 +0800

    fix: remove unnecessary async function definitions.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-# 声明：本代码仅供学习和研究目的使用。使用者应遵守以下原则：  
-# 1. 不得用于任何商业用途。  
-# 2. 使用时应遵守目标平台的使用条款和robots.txt规则。  
-# 3. 不得进行大规模爬取或对平台造成运营干扰。  
-# 4. 应合理控制请求频率，避免给目标平台带来不必要的负担。   
+# 声明：本代码仅供学习和研究目的使用。使用者应遵守以下原则：
+# 1. 不得用于任何商业用途。
+# 2. 使用时应遵守目标平台的使用条款和robots.txt规则。
+# 3. 不得进行大规模爬取或对平台造成运营干扰。
+# 4. 应合理控制请求频率，避免给目标平台带来不必要的负担。
 # 5. 不得用于任何非法或不当的用途。
-#   
-# 详细许可条款请参阅项目根目录下的LICENSE文件。  
-# 使用本代码即表示您同意遵守上述原则和LICENSE中的所有条款。  
+#
+# 详细许可条款请参阅项目根目录下的LICENSE文件。
+# 使用本代码即表示您同意遵守上述原则和LICENSE中的所有条款。
 
 
 import asyncio
@@ -28,14 +28,14 @@ from media_platform.zhihu import ZhihuCrawler
 
 
 class CrawlerFactory:
-    CRAWLERS: Dict[str, AbstractCrawler]= {
+    CRAWLERS: Dict[str, AbstractCrawler] = {
         constant.XHS_PLATFORM_NAME: XiaoHongShuCrawler,
         constant.WEIBO_PLATFORM_NAME: WeiboCrawler,
         constant.TIEBA_PLATFORM_NAME: TieBaCrawler,
         constant.BILIBILI_PLATFORM_NAME: BilibiliCrawler,
         constant.DOUYIN_PLATFORM_NAME: DouYinCrawler,
         constant.KUAISHOU_PLATFORM_NAME: KuaiShouCrawler,
-        constant.ZHIHU_PLATFORM_NAME: ZhihuCrawler
+        constant.ZHIHU_PLATFORM_NAME: ZhihuCrawler,
     }
 
     @staticmethod
@@ -48,14 +48,19 @@ class CrawlerFactory:
         Returns:
 
         """
-        crawler_class: Optional[Type[AbstractCrawler]] = CrawlerFactory.CRAWLERS.get(platform)
+        crawler_class: Optional[Type[AbstractCrawler]] = CrawlerFactory.CRAWLERS.get(
+            platform
+        )
         if not crawler_class:
-            raise ValueError("Invalid Media Platform Currently only supported xhs or dy or ks or bili ...")
+            raise ValueError(
+                "Invalid Media Platform Currently only supported xhs or dy or ks or bili ..."
+            )
         return crawler_class()
 
 
 async def main():
-    print("""
+    print(
+        """
 # ⚠️声明：本代码仅供学习和研究目的使用。使用者应遵守以下原则：  
 # ⚠️1. 不得用于任何商业用途。  
 # ⚠️2. 使用时应遵守目标平台的使用条款和robots.txt规则。  
@@ -65,10 +70,11 @@ async def main():
 # ⚠️  
 # ⚠️详细许可条款请参阅项目根目录下的LICENSE文件。  
 # ⚠️使用本代码即表示您同意遵守上述原则和LICENSE中的所有条款。
-""")
+"""
+    )
 
     # parse cmd
-    await cmd_arg.parse_cmd()
+    cmd_arg.parse_cmd()
 
     # init db
     if config.SAVE_DATA_OPTION == "db":
@@ -82,7 +88,7 @@ async def main():
         await db.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         # asyncio.run(main())
         asyncio.get_event_loop().run_until_complete(main())
